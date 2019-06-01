4bfc07748b99b7de65a1b40564074eb56939d889
Rodrigo Brechard <rodrigobrechard@gmail.com>
2019-06-01 11:52:54 +0200

    heatmap in experimentator with axis labels epipolar error introduced in run_grid_search

--- src/experimentator.py
+++ src/experimentator.py
@@ -1,20 +1,37 @@
-from scipy.spatial.distance import cdist
-import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 from ransac import *
 
 
-class Experimentator():
+def plot_heatmap(error_threshold_grid, error_threshold_results, i, number_of_accepted_points_grid, sample_size):
+    plt.subplot(1, 3, i)
+    sns.heatmap(error_threshold_results,
+                vmin=np.min(error_threshold_results), vmax=np.max(error_threshold_results),
+                xticklabels=number_of_accepted_points_grid, yticklabels=np.round(error_threshold_grid, 2),
+                linewidth=0.5)
+    plt.title("Sample size = " + str(sample_size))
+
+
+def calculate_distance(matches):
+    points_1 = matches[:, [0, 1]]
+    points_2 = matches[:, [2, 3]]
+    error = np.sqrt(np.sum(((points_1 - points_2) ** 2))) / len(matches)
+    return error
+
+
+def calculate_error_helper(matches, F):
+    keypoints = add_ones_column(matches[:, [0, 1]])
+    ground_truth = add_ones_column(matches[:, [2, 3]])
+    return np.sum(calculate_error(keypoints, F, ground_truth, method='sampson')) / len(matches)
+
+
+class Experimentator:
 
     def __init__(self, matches):
         self.matches = matches
 
-    def calculate_distance(self, matches):
-        points_1 = matches[:, [0, 1]]
-        points_2 = matches[:, [2, 3]]
-        error = np.sqrt(np.sum(((points_1 - points_2) ** 2))) / len(matches)
-        return error
-
-    def run_grid_search(self):
+    def run_grid_search(self, error_type="epipolar", plot=True):
         outlier_proportion = 0.2
 
         # Define grid
@@ -24,22 +41,32 @@ class Experimentator():
 
         final_results = []
 
-        for sample_size in sample_size_grid:
+        for grid_pos, sample_size in enumerate(sample_size_grid):
             number_of_iterations = calculate_number_of_iterations(sample_size, outlier_proportion)
             error_threshold_results = []
             for error_threshold in error_threshold_grid:
                 accepted_points_results = []
                 for number_of_accepted_points in number_of_accepted_points_grid:
                     # Average the error after running the algorithm several times
-                    times = 10
+                    times = 100
                     error = 0
                     for i in range(times):
-                        _, best_matches = run_ransac(self.matches, number_of_iterations, sample_size, error_threshold,
-                                                     number_of_accepted_points, speed_up = True)
-                        error += self.calculate_distance(best_matches)
+                        F, best_matches = run_ransac(self.matches, number_of_iterations, sample_size, error_threshold,
+                                                     number_of_accepted_points, speed_up=True)
+                        if error_type == "euclidean":
+                            error += calculate_distance(best_matches)
+                        else:
+                            error += calculate_error_helper(best_matches, F)
 
                     accepted_points_results.append(error / times)
                 error_threshold_results.append(accepted_points_results)
+
+            if plot:
+                plot_heatmap(error_threshold_grid, error_threshold_results, grid_pos + 1,
+                             number_of_accepted_points_grid, sample_size)
+
             final_results.append(error_threshold_results)
 
+        if plot:
+            plt.show()
         return final_results
--- src/main.py
+++ src/main.py
@@ -1,23 +1,11 @@
 from experimentator import *
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 # Load matches
 matches = np.loadtxt(
-    '/Users/erytheis/PycharmProjects/Computer Vision/Assignment 3/description/data/library/library_matches.txt')
+    '../description/data/library/library_matches.txt')
+    # '/Users/erytheis/PycharmProjects/Computer Vision/Assignment 3/description/data/library/library_matches.txt')
 
 experimentator = Experimentator(matches)
 results = experimentator.run_grid_search()
 
-i = 1
-
-for result in results:
-    plt.subplot(1, 3, i)
-    # plt.imshow(result, cmap='hot', interpolation='nearest')
-    sns.heatmap(result, linewidth = 0.5)
-    plt.title("Sample size = " + str(i + 7))
-    i += 1
-plt.show()
-
 # TODO 1)create experimentator class and calculate the error measure (A * F.reshape(9,1))
-
