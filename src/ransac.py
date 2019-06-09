Fixed A_f error calculation method

src/ransac.py
@@ -31,7 +31,16 @@ def fit_fundamental_matrix(matches):
     return F
 
 
-def run_ransac(matches, num_of_iterations, sample_size, error_threshold, num_of_accepted_points, use_T=True):
+def run_ransac(matches, num_of_iterations, sample_size, error_threshold, num_of_accepted_points):
+    """
+    Run RANSAC with a given set of parameters
+    :param matches: (n x 4) matches found by SIFT alglorithm
+    :param num_of_iterations: number of iterations
+    :param sample_size: number of elements sampled in one iteration
+    :param error_threshold: threshold of a good result
+    :param num_of_accepted_points: the number of points at which the model is considered good
+    :return: Fundamental matrix and the array of inliers
+    """
     max_number_of_inliers, F_best, best_matches = 0, 0, 0
     sample_batch = matches
 
@@ -44,7 +53,7 @@ def run_ransac(matches, num_of_iterations, sample_size, error_threshold, num_of_
         F = fit_fundamental_matrix(sample)
 
         # Compare the distance
-        error = calculate_error(matches, F, method='sampson')
+        error = calculate_error(matches, F, method = 'sampson')
 
         # Filter the points within the band width
         inliers_idx = np.where(error < error_threshold)[0]
@@ -58,14 +67,21 @@ def run_ransac(matches, num_of_iterations, sample_size, error_threshold, num_of_
 
         # If the number of inliers is bigger than the number of accepted points then this is a good model,
         # refit the model using all these inliers
-        if use_T and len(inliers) > num_of_accepted_points:
+        if  len(inliers) > num_of_accepted_points:
             sample_batch = inliers
 
     # print "Number of inliers " + str(max_number_of_inliers)
     return F_best, best_matches
 
 
-def calculate_error(matches, F, method='sampson'):
+def calculate_error(matches, F, method = 'a_f'):
+    """
+    A method that calculates the error of fundamental matrix with different methods
+    :param matches: (n x 4) array of correspondenses
+    :param F: (3 x 3) fundamental matrix
+    :param method: {sampson, a_f, euclidean)
+    :return: a vector of values of error
+    """
     err = 0
     x = add_ones_column(matches[:, [0, 1]]).T
     x_p = add_ones_column(matches[:, [2, 3]]).T
@@ -75,14 +91,13 @@ def calculate_error(matches, F, method='sampson'):
         denom = F1[0] ** 2 + F1[1] ** 2 + F2[0] ** 2 + F2[1] ** 2
         err = (np.diag(np.dot(x.T, np.dot(F, x_p)))) ** 2 / denom
     elif method == 'a_f':
-        A = build_A(matches)
-        err = abs(np.dot(A, F.reshape((9, 1))))
+        err = np.dot(x.T, np.dot(F, x_p))
     elif method == 'euclidean':
         err = np.sqrt(np.sum(((x - x_p) ** 2))) / len(matches)
     return err
 
 
-def calculate_number_of_iterations(s, e, p=0.99):
+def calculate_number_of_iterations(s, e, p = 0.99):
     """
     Calculate minimum number of required samplings in order to achieve not contaminated set of points
     :param s: number of samples in one sampling
@@ -110,12 +125,12 @@ def build_A(matches):
 
 def RANSAC_for_fundamental_matrix(matches):
     # Hyperparameters
-    sample_size = 9
+    sample_size = 10
     outlier_proportion = 0.6
     number_of_iterations = calculate_number_of_iterations(sample_size, outlier_proportion)
     # number_of_iterations = 2000
-    error_threshold = 1
-    number_of_accepted_points = 100
+    error_threshold = 0.08
+    number_of_accepted_points = 150
 
     print "Expected number of iteration = " + str(number_of_iterations)
     return run_ransac(matches, number_of_iterations, sample_size, error_threshold, number_of_accepted_points)